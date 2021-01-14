a0a3b302befe99c272da804ba271a7f49aa07884
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-14 13:34:20 -0500

    created basic server and empty arrays for tables & waitList

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 // REQUIRE EXPRESS
+const express = require("express");
 
 // CREATE AN INSTANCE OF EXPRESS
+const app = express();
 
 // CREATE A PORT FOR THE APP TO RUN
+const PORT = process.env.PORT || 8080;
 // BUT ALSO LISTEN TO ENVIRONMENT VARIABLES WE CAN DEPLOY TO HEROKU
 
 // ADD MIDDLEWARE
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // CREATE AN ARRAY OF TABLES
+const tables = [
+
+];
 // CREATE AN ARRAY OF WAITLIST
+const waitList = [
+
+];
+
 
 // ARRAYS OF OBJECTS. OBJECTS SHOULD HAVE
 // NAME
@@ -30,3 +40,6 @@
 // CREATE A TABLE/RESERVATION
 
 // LISTEN ON THE PORT
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
